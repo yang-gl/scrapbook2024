5ef2f47326a4246037098d07586b804fa09f4ff5
Ba Manzi <ba.manzi@yahoo.com>
2025-09-14 23:24:52 +0800

    First commit

---
+++ tree/meta.js
@@ -0,0 +1,236 @@
+/* Feel free to edit this file, but keep data code valid JSON format. */
+scrapbook.meta({
+  "20250914032246642": {
+    "index": "202509/20250914032246642-superhuang.feishu.cn/index.html",
+    "title": "AI编程蓝皮书2.0（更新Claude 4+MCP） - Feishu Docs",
+    "type": "",
+    "create": "20250914032246642",
+    "modify": "20250914032246642",
+    "source": "https://superhuang.feishu.cn/wiki/CBBPwvgEuicVhFkx0s7cPmhpn4e",
+    "icon": "icon_file_doc_nor-256x256.5ecc0ce2486516290b43.png",
+    "comment": "",
+    "parent": "root",
+    "recycled": "20250914123703283"
+  },
+  "20250914123600496": {
+    "title": "AI coding",
+    "type": "folder",
+    "create": "20250914123600496",
+    "modify": "20250914123600496"
+  },
+  "20250914123648447": {
+    "index": "",
+    "title": "AI编程蓝皮书2.0（更新Claude 4+MCP） - Feishu Docs",
+    "type": "bookmark",
+    "create": "20250914123648447",
+    "modify": "20250914123648447",
+    "source": "https://superhuang.feishu.cn/wiki/CBBPwvgEuicVhFkx0s7cPmhpn4e",
+    "icon": "../tree/favicon/5d7845ca4b41ce09749a42a7cc1d76e1b30e08e0.ico",
+    "comment": ""
+  },
+  "20250914123917592": {
+    "index": "202509/20250914123917592-x.com/index.html",
+    "title": "Tw93 on X: \"深度对比了一天 ChatGPT 的 Codex 和 Claude Code，早上刚开始继续包月 ChatGPT Plus 的的时候，安装完 VSCode 的 Codex 体验下来，第一感觉是，Codex 的产品交互、模块展示丰富上完全强于 Claude Code，每个步骤展示也清晰的，代码改动对比也舒服的，而且甚至用了很久都没有之前的 Claude Code https://t.co/YfSha1AcSs\" / X",
+    "type": "",
+    "create": "20250914123917592",
+    "modify": "20250914123917592",
+    "source": "https://x.com/HiTw93/status/1966854153503904170",
+    "icon": "twitter.3.ico",
+    "parent": "20250914123600496",
+    "recycled": "20250914130049196"
+  },
+  "20250914124008750": {
+    "title": "AI Agent",
+    "type": "folder",
+    "create": "20250914124008750",
+    "modify": "20250914124008750"
+  },
+  "20250914124103875": {
+    "index": "202509/20250914124103875-baoyu.io/index.html",
+    "title": "构建高效 Agent [译] | 宝玉的分享",
+    "type": "",
+    "create": "20250914124103875",
+    "modify": "20250914124103875",
+    "source": "https://baoyu.io/translations/building-effective-agents",
+    "icon": "urn:scrapbook:download:error:https://baoyu.io/favicon-16x16.png"
+  },
+  "20250914124146160": {
+    "index": "202509/20250914124146160-x.com/index.html",
+    "title": "yousa on X: \"分享个自己昨晚的aha moment 昨晚翻阅了一下Claude半年前发的一篇文章，关于AI Agent如何构建。 https://t.co/PSYLS0ADo5 这篇文章主要介绍了AI Agent构建的一些基本逻辑单元，读的时候发现自己日常prompt开发也是会用这些做法。重新温习了一遍 要点： 1.\" / X",
+    "type": "",
+    "create": "20250914124146160",
+    "modify": "20250914124146160",
+    "source": "https://x.com/y0usali/status/1966806342527857053",
+    "icon": "twitter.3.ico"
+  },
+  "20250914124203749": {
+    "index": "202509/20250914124203749-www.anthropic.com/index.html",
+    "title": "Building Effective AI Agents \\ Anthropic",
+    "type": "",
+    "create": "20250914124203749",
+    "modify": "20250914124203749",
+    "source": "https://www.anthropic.com/engineering/building-effective-agents",
+    "icon": "favicon.ico"
+  },
+  "20250914124325599": {
+    "index": "202509/20250914124325599-threadreaderapp.com/index.html",
+    "title": "Thread by @dotey: 如果你的 Agent 还要用 ReAct 框架写 Prompt，那么要么说明",
+    "type": "",
+    "create": "20250914124325599",
+    "modify": "20250914124325599",
+    "source": "https://threadreaderapp.com/thread/1966689857863913597.html",
+    "icon": "favicon-32x32.png"
+  },
+  "20250914124350271": {
+    "index": "202509/20250914124350271-baoyu.io/index.html",
+    "title": "为 AI 智能体打造高效工具——让 AI 智能体来帮忙 | 宝玉的分享",
+    "type": "",
+    "create": "20250914124350271",
+    "modify": "20250914124350271",
+    "source": "https://baoyu.io/translations/writing-tools-for-agents",
+    "icon": "urn:scrapbook:download:error:https://baoyu.io/favicon-16x16.png"
+  },
+  "20250914124439824": {
+    "index": "202509/20250914124439824-www.anthropic.com/index.html",
+    "title": "Writing effective tools for AI agents—using AI agents \\ Anthropic",
+    "type": "",
+    "create": "20250914124439824",
+    "modify": "20250914124439824",
+    "source": "https://www.anthropic.com/engineering/writing-tools-for-agents",
+    "icon": "favicon.ico"
+  },
+  "20250914124608496": {
+    "index": "202509/20250914124608496-baoyu.io/index.html",
+    "title": "Claude Code 为何如此强大？Anthropic 万字长文揭秘 AI Agent 工具开发五大“心法” | 宝玉的分享",
+    "type": "",
+    "create": "20250914124608496",
+    "modify": "20250914124608496",
+    "source": "https://baoyu.io/blog/claude-code-anthropic-ai-agent-development",
+    "icon": "urn:scrapbook:download:error:https://baoyu.io/favicon-16x16.png"
+  },
+  "20250914124905407": {
+    "index": "202509/20250914124905407-x.com/index.html",
+    "title": "宝玉 on X: \"ChatGPT 和 Claude 都有记忆功能，但两者实现原理截然不同。 以前研究 Claude Code 的时候，发现它不是基于 RAG 而是基于 grep 工具使用正则检索代码，没想到 Anthropic 把这一点进一步发扬光大，在 Claude 网页版的记忆功能中，也是基于实时搜索来做的，跟 ChatGPT 的记忆功能思路完全不一样。 https://t.co/ehBCV2aCTa\" / X",
+    "type": "",
+    "create": "20250914124905407",
+    "modify": "20250914124905407",
+    "source": "https://x.com/dotey/status/1966625643635843083",
+    "icon": "twitter.3.ico",
+    "parent": "20250914124008750",
+    "recycled": "20250914125948114"
+  },
+  "20250914125116366": {
+    "index": "202509/20250914125116366-threadreaderapp.com/index.html",
+    "title": "Thread by @shao__meng : Claude Code is not just for coding. It's an AI agent that can do whatever you want it to do.",
+    "type": "",
+    "create": "20250914125116366",
+    "modify": "20250914125116366",
+    "source": "https://threadreaderapp.com/thread/1966303982407811474.html",
+    "icon": "favicon-32x32.png"
+  },
+  "20250914125932738": {
+    "index": "202509/20250914125932738-threadreaderapp.com/index.html",
+    "title": "Thread by @dotey: ChatGPT 和 Claude 都有记忆功能，但两者实现原理截然不同。",
+    "type": "",
+    "create": "20250914125932738",
+    "modify": "20250914125932738",
+    "source": "https://threadreaderapp.com/thread/1966625643635843083.html",
+    "icon": "favicon-32x32.png"
+  },
+  "20250914130005895": {
+    "index": "202509/20250914130005895-threadreaderapp.com/index.html",
+    "title": "Thread by @HiTw93: 深度对比了一天 ChatGPT 的 Codex 和 Claude Code",
+    "type": "",
+    "create": "20250914130005895",
+    "modify": "20250914130005895",
+    "source": "https://threadreaderapp.com/thread/1966854153503904170.html",
+    "icon": "favicon-32x32.png"
+  },
+  "20250914130350223": {
+    "index": "202509/20250914130350223-x.com/index.html",
+    "title": "meng shao on X: \"给 AI 智能体开发者的实用建议 —— @AgnoAgi 创始人 @ashpreetbedi 现在的 AI https://t.co/jjQRpYi7la\" / X",
+    "type": "",
+    "create": "20250914130350223",
+    "modify": "20250914130350223",
+    "source": "https://x.com/shao__meng/status/1967006948618104882",
+    "icon": "twitter-pip.3.ico"
+  },
+  "20250914130459325": {
+    "index": "202509/20250914130459325-threadreaderapp.com/index.html",
+    "title": "Thread by @petergyang : Claude Code is not just for coding. It's an AI agent that can do whatever you want it to do.",
+    "type": "",
+    "create": "20250914130459325",
+    "modify": "20250914130459325",
+    "source": "https://threadreaderapp.com/thread/1964697797909000439.html",
+    "icon": "favicon-32x32.png"
+  },
+  "20250914130554504": {
+    "index": "202509/20250914130554504-baoyu.io/index.html",
+    "title": "Claude Code 究竟牛在哪里？（以及如何在你的 AI 智能体中复刻它的魔法！） | 宝玉的分享",
+    "type": "",
+    "create": "20250914130554504",
+    "modify": "20250914130554504",
+    "source": "https://baoyu.io/translations/decoding-claude-code",
+    "icon": "urn:scrapbook:download:error:https://baoyu.io/favicon-16x16.png"
+  },
+  "20250914145031886": {
+    "index": "202509/20250914145031886-threadreaderapp.com/index.html",
+    "title": "Thread by @dotey: 如果你对 Claude Code 的工作原理感兴趣，这篇文章一定要读一下",
+    "type": "",
+    "create": "20250914145031886",
+    "modify": "20250914145031886",
+    "source": "https://threadreaderapp.com/thread/1959460479232847935.html",
+    "icon": "favicon-32x32.png"
+  },
+  "20250914145252645": {
+    "index": "202509/20250914145252645-threadreaderapp.com/index.html",
+    "title": "Thread by @oasisfeng: 其实从 function call 到 MCP，再到 CLI，背后都遵循着一条主线 —— 为 agent 不断优化多工具调用",
+    "type": "",
+    "create": "20250914145252645",
+    "modify": "20250914145252645",
+    "source": "https://threadreaderapp.com/thread/1940679704572580002.html",
+    "icon": "favicon-32x32.png"
+  },
+  "20250914145335226": {
+    "title": "Tool Calling",
+    "type": "folder",
+    "create": "20250914145335226",
+    "modify": "20250914145335226"
+  },
+  "20250914145352909": {
+    "index": "202509/20250914145352909-threadreaderapp.com/index.html",
+    "title": "Thread by @cryptonerdcn: tool-call 都是分显式和隐式两种的",
+    "type": "",
+    "create": "20250914145352909",
+    "modify": "20250914145352909",
+    "source": "https://threadreaderapp.com/thread/1945045817338826862.html",
+    "icon": "favicon-32x32.png"
+  },
+  "20250914145449284": {
+    "index": "202509/20250914145449284-threadreaderapp.com/index.html",
+    "title": "Thread by @ashpreetbedi: Pro Tip for Agent Builders",
+    "type": "",
+    "create": "20250914145449284",
+    "modify": "20250914145449284",
+    "source": "https://threadreaderapp.com/thread/1966895931531366690.html",
+    "icon": "favicon-32x32.png"
+  },
+  "20250914145738606": {
+    "index": "202509/20250914145738606-threadreaderapp.com/index.html",
+    "title": "Thread by @Yangyixxxx: 重温一下Anthropic 发布了的指南：“如何构建有效的 AI Agents”",
+    "type": "",
+    "create": "20250914145738606",
+    "modify": "20250914145738606",
+    "source": "https://threadreaderapp.com/thread/1896795276117205056.html",
+    "icon": "favicon-32x32.png"
+  },
+  "20250914150500933": {
+    "index": "202509/20250914150500933-threadreaderapp.com/index.html",
+    "title": "Thread by @aakashg0: This guy literally shared a step-by-step roadmap to build your first AI agent",
+    "type": "",
+    "create": "20250914150500933",
+    "modify": "20250914150500933",
+    "source": "https://threadreaderapp.com/thread/1967135994228166848.html",
+    "icon": "favicon-32x32.png"
+  }
+})
